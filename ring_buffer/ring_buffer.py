d229edafb62f3129f61be987c515ed5cab645b8f
Diana Grams <deegrams221@gmail.com>
2020-01-31 09:22:02 -0700

    added notes and psuedocode to ring_buffer.py to get started

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,24 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # pass
+        # using recursion
+        # From README: When the ring buffer is full and a new element is inserted, the oldest element in the ring buffer is overwritten with the newest element.
+        # if storage is not full, add to tail and updated the current
+        # if storage is full, remove the head(oldest) to free up space, add item to tail(newest)
+            # if current is at the head, set the current to tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        # set the start to current and append value to contents
+        # loop through the nodes and append values
+            # if start.next, set nextItem to start next, else set nextItem to storage head
+            # while nextItem does not equal start, append nextItem value to contents
+                # if nextItem then set nextItem to nextItem next, else nextItem to storage head
+            # return contents
 
         return list_buffer_contents
 
